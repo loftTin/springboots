8c0065368580ae3d1bab516e1ea835cd4b8328a8
baifan06 <1280887137@qq.com>
2023-07-12 10:24:11 +0800

    a version with login/logout and without admin

--- blog-vue/src/components/friends.vue
+++ blog-vue/src/components/friends.vue
@@ -1,36 +1,34 @@
 <!-- 友情链接模块 -->
 <template>
-<div class="tFriendsBox">
-    <h1>友链申请格式
-        <h3>        网站名称: 三更博客</h3>
-        <h3>网站地址: https://www.baidu.com </h3>
-        <h3>网站描述: 百度你懂的</h3>
-        <h3>网站logo: https://www.sg.com/1.png</h3>
-
-</h1>
-    <el-row>
-        <el-col :span="12" class="tf-item" v-for="(item,index) in friendslink" :key="'f'+index">
-            <a :href="item.address" target="_blank">
-                <img :src="item.logo?item.logo:'static/img/tou.jpg'"  :onerror="$store.state.errorImg">
-                <h4>{{item.name}}</h4>
-                <p>{{item.description}}</p>
-            </a>
-        </el-col>
-    </el-row>
-</div>
+    <div class="tFriendsBox">
+        <h1>友链申请格式</h1>
+        <h3>网站名称：Clarkson's Farm</h3>
+        <h3>网站地址: https://www.bilibili.com/video/BV1F54y1K76g/?spm_id_from=333.999.0.0 </h3>
+        <h3>网站描述: 英国最著名但最不合格的业余农民杰里米·克拉克森回归了！“一毛之地”农场迎来了新一年...</h3>
+        <h3>网站logo: http://rxauma9r3.hb-bkt.clouddn.com/link01.jpg</h3>
+        <el-row>
+            <el-col :span="12" class="tf-item" v-for="(item, index) in friendslink" :key="'f' + index">
+                <a :href="item.address" target="_blank">
+                    <img :src="item.logo ? item.logo : 'static/img/tou.jpg'" :onerror="$store.state.errorImg">
+                    <h4>{{ item.name }}</h4>
+                    <p>{{ item.description }}</p>
+                </a>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-import {getAllLink} from '../api/link.js'
+import { getAllLink } from '../api/link.js'
 export default {
     data() { //选项 / 数据
         return {
-            friendslink:[]//友情链接
+            friendslink: []//友情链接
         }
     },
     methods: { //事件处理器
-        getList(){
-            getAllLink().then((response)=>{
+        getList() {
+            getAllLink().then((response) => {
                 this.friendslink = response
             })
         }
@@ -45,56 +43,62 @@ export default {
 </script>
 
 <style>
-.tFriendsBox{
+.tFriendsBox {
     background: #fff;
-    padding:15px;
+    padding: 15px;
     border-radius: 5px;
     margin-bottom: 40px;
 }
-.tFriendsBox h1{
-    padding:15px 0;
+
+.tFriendsBox h1 {
+    padding: 15px 0;
     font-size: 25px;
     font-weight: bold;
 }
-.tFriendsBox .tf-item{
+
+.tFriendsBox .tf-item {
     transition: all 0.3s ease-out;
     border-radius: 5px;
     position: relative;
 }
-.tFriendsBox .tf-item:hover{
-    background: rgba(230,244,250,.5);
+
+.tFriendsBox .tf-item:hover {
+    background: rgba(230, 244, 250, .5);
 }
-.tFriendsBox .tf-item a{
+
+.tFriendsBox .tf-item a {
     display: block;
-    padding:0 10px 0 90px;
-    height:90px;
+    padding: 0 10px 0 90px;
+    height: 90px;
 }
-.tFriendsBox .tf-item a img{
-    width:60px;
-    height:60px;
+
+.tFriendsBox .tf-item a img {
+    width: 60px;
+    height: 60px;
     border-radius: 50%;
     position: absolute;
-    top:15px;
-    left:15px;
+    top: 15px;
+    left: 15px;
     cursor: pointer;
     object-fit: cover;
 }
-.tFriendsBox .tf-item a h4{
+
+.tFriendsBox .tf-item a h4 {
     cursor: pointer;
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
     font-size: 20px;
-    padding-top:15px;
+    padding-top: 15px;
     font-weight: bold;
 }
-.tFriendsBox .tf-item a p{
-    margin:10px 0;
+
+.tFriendsBox .tf-item a p {
+    margin: 10px 0;
     font-size: 12px;
     line-height: 24px;
-    color:#999;
+    color: #999;
     cursor: pointer;
     white-space: nowrap;
     text-overflow: ellipsis;
-}
-</style>
+}</style>
--- blog-vue/src/components/reward.vue
+++ blog-vue/src/components/reward.vue
@@ -3,70 +3,57 @@
   <div class="tRewardBox tcommonBox">
     <header>
       <h1>
-        <a href="#/DetailShare" target="_blank"> 赞赏 </a>
+        <a href="#/DetailShare" target="_blank"> 鸣谢 </a>
       </h1>
     </header>
     <section>
       <div>
-        <img
-          src="static/img/coffee.jpg"
-          alt=""
-          style="max-width: 20%"
-          class="coffee"
-        />
+        <img src="static/img/coffee.jpg" alt="" style="max-width: 20%" class="coffee" />
       </div>
-      <h1>赞赏说明：</h1>
+      <h1>特别鸣谢：</h1>
       <p>
-        如果我的课程对你有所帮助，你可以请作者喝杯咖啡表示鼓励
-        ☕️)请备注留下你的姓名/昵称方便我公布赞赏记录。
+
+        ---<br>
+
+        我们要衷心感谢以下人士和组织，没有他们的支持和帮助，我们的个人博客网站将无法顺利搭建和运行。<br>
+
+        首先，感谢肖老师、感谢《JAVA系统分析与架构设计》，在我们搭建个人博客的过程中提供的技术指导和宝贵建议。<br>
+
+        感谢开源社区。感谢所有开源项目的贡献者，为我们个人博客网站的搭建提供了便捷的版本控制，你们的无私奉献使得我能够使用高质量的工具和技术来构建我们的个人博客网站。你们的工作为我提供了强大的功能和灵活的扩展性。<br>
+
+        感谢互联网上无数博客作者和技术专家。你们的分享和教程对我们在个人博客搭建过程中的学习起到了重要作用。无论是技术问题的解答还是实用建议，你们的经验和知识都给予了我们巨大的帮助。<br>
+
+        再次向以上所有人表示最衷心的感谢！<br>
+
+        ---<br>
       </p>
       <hr />
       <h1>赞赏方式：</h1>
       <el-row :gutter="30">
         <el-col :span="12" class="donate-item">
           <div class="donate-tip">
-            <img
-              :src="
-                rewardData.wechat_image
-                  ? rewardData.wechat_image
-                  : 'static/img/wx_pay.png'
-              "
-              :onerror="$store.state.errorImg"
-            />
+            <img :src="rewardData.wechat_image
+                ? rewardData.wechat_image
+                : 'static/img/wx_pay.png'
+              " :onerror="$store.state.errorImg" />
             <span>微信扫一扫，请我喝杯咖啡</span>
           </div>
         </el-col>
         <el-col :span="12" class="donate-item">
           <div class="donate-tip">
-            <img
-              :src="
-                rewardData.alipay_image
-                  ? rewardData.alipay_image
-                  : 'static/img/ali_pay.jpg'
-              "
-              :onerror="$store.state.errorImg"
-            />
+            <img :src="rewardData.alipay_image
+                ? rewardData.alipay_image
+                : 'static/img/ali_pay.jpg'
+              " :onerror="$store.state.errorImg" />
             <span style="padding-top: 14px">支付宝扫一扫，请我喝杯咖啡</span>
           </div>
         </el-col>
       </el-row>
       <h1>@赞赏记录：</h1>
       <el-table :data="tableData" border style="width: 100%">
-        <el-table-column
-          prop="payTime"
-          label="日期"
-          align="center"
-        ></el-table-column>
-        <el-table-column
-          prop="name"
-          label="赞赏人"
-          align="center"
-        ></el-table-column>
-        <el-table-column
-          prop="money"
-          label="金额"
-          align="center"
-        ></el-table-column>
+        <el-table-column prop="payTime" label="日期" align="center"></el-table-column>
+        <el-table-column prop="name" label="赞赏人" align="center"></el-table-column>
+        <el-table-column prop="money" label="金额" align="center"></el-table-column>
       </el-table>
     </section>
   </div>
@@ -81,19 +68,19 @@ export default {
       tableData: [
         {
           //赞赏数据
-          "payTime":"2021-10-23",
-          "name":"智障少女欢乐多",
-          "money":"188"
+          "payTime": "2021-10-23",
+          "name": "智障少女欢乐多",
+          "money": "188"
         },
         {
-          "payTime":"2021-10-28",
-          "name":"刺猬",
-          "money":"50"
+          "payTime": "2021-10-28",
+          "name": "刺猬",
+          "money": "50"
         },
         {
-          "payTime":"2021-11-03",
-          "name":"迅捷小鹿",
-          "money":"30"
+          "payTime": "2021-11-03",
+          "name": "迅捷小鹿",
+          "money": "30"
         },
       ],
     };
@@ -119,6 +106,7 @@ export default {
 .tRewardBox section {
   padding-bottom: 20px;
 }
+
 .tRewardBox section h1 {
   margin: 10px 0;
   font-size: 25px;
@@ -126,10 +114,12 @@ export default {
   /*text-align: center;*/
   line-height: 30px;
 }
+
 .tRewardBox section p {
   margin: 10px 0;
   line-height: 24px;
 }
+
 .tRewardBox section hr {
   background: #ccc;
   margin-bottom: 30px;
@@ -139,15 +129,18 @@ export default {
   text-align: right;
   color: #44b549;
 }
+
 .tRewardBox .donate-item:last-child {
   text-align: left;
   color: #00a0e9;
 }
+
 .tRewardBox .donate-item img {
   width: 100%;
   display: block;
   height: auto;
 }
+
 .tRewardBox .donate-item div {
   display: inline-block;
   width: 150px;
@@ -155,16 +148,19 @@ export default {
   text-align: center;
   box-sizing: border-box;
 }
+
 .tRewardBox .donate-item div span {
   display: inline-block;
   width: 100%;
   margin: 10px 0;
   text-align: center;
 }
+
 .tRewardBox .el-table__body-wrapper {
   overflow: hidden;
 }
-.el-table--enable-row-hover .el-table__body tr:hover > td {
+
+.el-table--enable-row-hover .el-table__body tr:hover>td {
   background: transparent;
 }
 </style>
